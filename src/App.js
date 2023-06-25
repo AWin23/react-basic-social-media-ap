91e616b7bf7d43b1348bb2977426921a658825ac
Andrew Nguyen <nguyendrew20@gmail.com>
2023-06-25 15:34:05 -0700

    episode 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App"> 
+    <div>
+      <User name="Pedro" age={21} email="pedro@pedro.com" />
+      <User name="Andrew" age={21} email="andrew@andrew.com"  />
+      <User name="Dominic" age={4} email="dom@dom.com"  />
     </div>
+    <div>
+      <JobInfo salary={90000} position="Senior SDE" company="Amazon" />
+      <JobInfo salary={12000} position="Junior SDE" company="Google" />
+      <JobInfo salary={90000} position="Project Manager" company="Netflix" />
+    </div>
+  </div>
   );
+}
+
+//  this is a REACT component. 
+//  components will alwayas be returning UI STUFF like h1, divs, etc
+//  React Components always start wit uppercase letter
+const User = (props) => {
+  return( 
+  <div>
+    <h1>{props.name}</h1>
+    <h1>{props.age}</h1>
+    <h1>{props.email}</h1>
+  </div>
+  );
+}
+
+const JobInfo = (props) => {
+  return(
+    <div>
+      <h1>{props.salary}</h1>
+      <h1>{props.position}</h1>
+      <h1>{props.company}</h1>
+    </div>
+  )
 }
 
 export default App;
