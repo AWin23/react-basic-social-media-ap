17683696abe6f2c70f8546ee11a9cc48d8c569a8
Andrew Nguyen <nguyendrew20@gmail.com>
2023-06-25 17:47:57 -0700

    episode 3

--- src/App.js
+++ src/App.js
@@ -1,44 +1,27 @@
-import './App.css';
+import "./App.css";
 
-function App() {
+function App() {  
+  const Users = [
+    { name: "Jush", age: 19  },
+    { name: "Andrew", age: 21  },
+    { name: "Dom", age: 15 },
+    { name: "Blake", age: 14 },
+    { name: "Chris", age: 18 },
+  ];
+
+  //  write code to loop thru each of these planets and dispaly their names.
+  //  display names only if they are a gas giant
 
   return (
-  <div className="App"> 
-    <div>
-      <User name="Pedro" age={21} email="pedro@pedro.com" />
-      <User name="Andrew" age={21} email="andrew@andrew.com"  />
-      <User name="Dominic" age={4} email="dom@dom.com"  />
-    </div>
-    <div>
-      <JobInfo salary={90000} position="Senior SDE" company="Amazon" />
-      <JobInfo salary={12000} position="Junior SDE" company="Google" />
-      <JobInfo salary={90000} position="Project Manager" company="Netflix" />
-    </div>
+  <div className="App">
+     {/* loop thru each of the Users */}
+     {/* ask each Users using the KEY IF the User is OF AGE */}
+     {/*  You have to wrap .map or any JavaScript expressoin wit curly braces to indicate that its a JavaScript expressoin that needs to be evaluated */}
+    {Users.map(
+      (person, key) => {if (person.age >= 18) return <h1> {person.name}</h1>;    {/* then return the name of users who are OF AGE accessing it using the key */}
+      })}
   </div>
   );
 }
-
-//  this is a REACT component. 
-//  components will alwayas be returning UI STUFF like h1, divs, etc
-//  React Components always start wit uppercase letter
-const User = (props) => {
-  return( 
-  <div>
-    <h1>{props.name}</h1>
-    <h1>{props.age}</h1>
-    <h1>{props.email}</h1>
-  </div>
-  );
-}
-
-const JobInfo = (props) => {
-  return(
-    <div>
-      <h1>{props.salary}</h1>
-      <h1>{props.position}</h1>
-      <h1>{props.company}</h1>
-    </div>
-  )
-}
-
+  
 export default App;
