6694aed0ee0beff5d9cec1106bcc3eee6ecf14b1
Andrew Nguyen <nguyendrew20@gmail.com>
2023-07-01 14:00:11 -0700

    episode 4 hehe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
 import "./App.css";
+import React, { useState } from 'react';
 
+
+//  "States" are just a variable that holds data.
+//  STATE (whoel point) is a variable that when its value changes, React will be notified and LOOK to see if the value is DIFF than before 
+//  IF IT IS, it will trigger the UI to re-render .
+//  to REDISPLAY the WHOLE UI
 function App() {
+  const [count, setCount] = useState(0);  //  initial value providied in 'useState' DETERMINES the type of state variables
+  //  so now setCount has to take in AN INT. 
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  }
+
+  const setToZero = () => {
+    setCount(0);
+  }
+
+  
   return (
     <div className="App">
-      <Job salary={90000} position="Senior SDE" company="Amazon" />
-      <Job salary={12000} position="Junior SDE" company="Google" />
-      <Job salary={10000} position="Project Manager" company="Netflix" />
-    </div>
-  );
-}
+      <button
+        onClick={() => {
+          increment();
+        }}>
+        Increase </button>
 
-const Job = (props) => {
-  return (
-    <div>
-      <h1> {props.salary}</h1>
-      <h1> {props.position}</h1>
-      <h1> {props.company}</h1>
+      <button onClick={() => {
+        decrement();
+      }}
+      >
+        Decrease
+      </button>
+
+      <button onClick={() => {
+        setToZero();
+      }}> Set to Zero </button>
+
+      {count}
     </div>
   );
-};
+  
+  //  THIS WAS MY INITIAL SOLUTION BUT THE ONE SHOWN IS THE OPTIMIZED SOLUTION. I initally called the setCounts in the onClick which can work but its not as clean ig? 
+  
+  // return (
+  //   <div className="App">
+  //     <button
+  //       onClick ={
+  //       setCount(count + 1)
+  //       }
+  //     >
+  //       Increase </button>
+  
+  //     <button onClick={
+  //       setCount(count - 1)
+  //     }
+  //     >
+  //       Decrease
+  //     </button>
+  
+  //     <button onClick={
+  //       setCount(0)
+  //     }> Set to Zero </button>
+  
+  //     {count}
+  //   </div>
+  // );
+
+}
 
 export default App;
